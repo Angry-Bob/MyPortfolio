174e659205e356a6b5ec101d40068246bd74fc36
Robert <rcastor0000@gmail.com>
2024-01-15 00:27:28 -0600

    added components for portfolio

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+
+function App() {
+
+
+  return (
+    <>
+    <About />
+    <Contact />
+    <Projects />
+    </>
+  )
+}
+
+export default App
