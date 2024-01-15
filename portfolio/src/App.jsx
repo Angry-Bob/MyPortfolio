5789435532a1a62e547b1954318bed38f01c1b3e
Robert <rcastor0000@gmail.com>
2024-01-15 00:34:16 -0600

    fixed components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,18 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import About from './components/About'
-import Contact from './components/Contact'
-import Projects from './components/Projects'
+import Nav from './components/Nav'
 
 function App() {
 
 
   return (
     <>
-    <About />
-    <Contact />
-    <Projects />
+    <Nav />
     </>
   )
 }
--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import About from './About'
+import Projects from './Projects'
+import Contact from './Contact'
 
 function Nav() {
   return (
+    <>
+
     <div>Nav</div>
+    <div>__________</div>
+    <About />
+    <Contact />
+    <Projects />
+
+    </>
   )
 }
 
