4bec9f537dc3d306d6ea2eb3bb84418738a778b7
Robert <rcastor0000@gmail.com>
2024-01-15 23:24:04 -0600

    Added links

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -6,6 +6,7 @@ function About() {
     <h1>Hello, I'm Robert Castor!</h1>
     <h2>Software Engineer</h2>
     <p>Information about me will go in this section.</p>
+    <div className='links'>Resume Github LinkedIn</div>
     </>
   )
 }
