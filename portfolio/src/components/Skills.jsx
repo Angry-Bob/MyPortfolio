46229ad35f57208ecf37c99c6d47a815461e26f5
Robert <rcastor0000@gmail.com>
2024-01-17 14:57:40 -0600

    added skills

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -5,7 +5,18 @@ function Skills() {
     <div className='skillsDiv'>
       
       <h3>Skills</h3>
-      
+      <ul>
+        <li>Javascript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>ReactJS</li>
+        <li>Python</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li>NodeJS</li>
+        <li>MYSQL</li>
+      </ul>
     </div>
   )
 }
